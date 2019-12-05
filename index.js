e098f10712d8fd84f4ae8af6af8baa0dca19b65d
Peter Rufo <peter@peterrufo.com>
2019-12-05 13:40:28 -0500

    Added toggle

--- index.js
+++ index.js
@@ -18,32 +18,42 @@ function displayResults(responseJson) {
       `<div class="thumbs">
       <h3>${responseJson.drinks[i].strDrink}</h3>
             <a href="javascript:displayCocktail(responseJson)"><img src="${responseJson.drinks[i].strDrinkThumb}" class="drink-image"></a>
-            </div>
+            
             <div class="details" id="${responseJson.drinks[i].idDrink}">
-            <h3>Instructions:</h3>
-            <p>${responseJson.drinks[i].idDrink}</p>
-            <p>${responseJson.drinks[i].strInstructions}</p>
-            <ul>Ingredients:
+            <ul class="js-ul"><h3>Ingredients:</h3>
               <li>${responseJson.drinks[i].strIngredient1}</li>
               <li>${responseJson.drinks[i].strIngredient2}</li>
               <li>${responseJson.drinks[i].strIngredient3}</li>
-               <li>${responseJson.drinks[i].strIngredient4}</li>
-             </ui>
-             </div>
-           `
+              <li>${responseJson.drinks[i].strIngredient4}</li>
+              <li>${responseJson.drinks[i].strIngredient5}</li>
+              <li>${responseJson.drinks[i].strIngredient6}</li>
+              <li>${responseJson.drinks[i].strIngredient7}</li>
+              <li>${responseJson.drinks[i].strIngredient8}</li>
+              <li>${responseJson.drinks[i].strIngredient9}</li>
+              <li>${responseJson.drinks[i].strIngredient10}</li>
+              <li>${responseJson.drinks[i].strIngredient11}</li>
+              <li>${responseJson.drinks[i].strIngredient12}</li>
+              <li>${responseJson.drinks[i].strIngredient13}</li>
+              <li>${responseJson.drinks[i].strIngredient14}</li>
+              <li>${responseJson.drinks[i].strIngredient15}</li>
+            </ul>
+          
+            <h3>Instructions:</h3>
+            <p>${responseJson.drinks[i].strInstructions}</p>
+            </div>
+      </div>
+      `
+           
+           
     );}
   $('#results').removeClass('hidden');
   $('.details').hide();
 }
 
 function displayCocktail(responseJson){
-  $('#results-list').on('click', '.drink-image', event => {
-    // alert(event.target.id);
-    $('.details').toggle(1000);
-    
-    
-    
-
+  $('#results-list').on('click', '.drink-image', function (event) {
+    console.log($(this).parent().siblings()[1]);
+    $(this).parent().siblings().toggle(1000);   
   });
 }
 
