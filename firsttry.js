690395c246a8f551fe4d2865064a898d395904a5
Peter Rufo <peter@peterrufo.com>
2019-12-05 10:44:24 -0500

    Displaying the instructions

---
+++ firsttry.js
@@ -0,0 +1,128 @@
+'use strict';
+
+const apiKey = '1';
+const searchIngredientURL = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php';
+const searchNameURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php';
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+  return queryItems.join('&');
+}
+  
+function displayIngredientResults(responseJson) {
+
+  console.log(responseJson);
+  $('#results-list').empty();
+  for (let i = 0; i < responseJson.drinks.length; i++){   
+    $('#results-list').append(
+      `<li><h3>${responseJson.drinks[i].strDrink}</h3>
+          <img src="${responseJson.drinks[i].strDrinkThumb}">
+          </li>`
+    );}
+  
+  $('#results').removeClass('hidden');
+}
+function displayNameResults(responseJson) {
+
+  console.log(responseJson);
+  $('#results-list').empty();
+  $('#js-error-message').empty();
+  for (let i = 0; i < responseJson.drinks.length; i++){   
+    $('#results-list').append(
+      `<h3>${responseJson.drinks[i].strDrink}</h3>
+            <img src="${responseJson.drinks[i].strDrinkThumb}">
+           `
+    );}
+    
+  $('#results').removeClass('hidden');
+}
+// function displayCocktail(responseJson) {
+//     $('#results-list').empty();
+//     $('#results-drink').empty();
+//     for (let i = 0; i < responseJson.drinks.length; i++){   
+//       $('#results-drink').append(
+//         `<h3>${responseJson.drinks[i].strDrink}</h3>
+//             <img src="${responseJson.drinks[i].strDrinkThumb}">
+//             <ul>Ingredients:
+//               <li>${responseJson.drinks[i].strIngredient1}</li>
+//               <li>${responseJson.drinks[i].strIngredient2}</li>
+//               <li>${responseJson.drinks[i].strIngredient3}</li>
+//               <li>${responseJson.drinks[i].strIngredient4}</li>
+//             </ui>
+//             <h3>Instructions:</h3>
+//             <p>${responseJson.drinks[i].strInstructions}</p>`
+//       );}
+    
+//     $('#results-drink').removeClass('hidden');
+  
+//   }
+    
+function searchByIngredients(query) {
+  const params = {
+    key: apiKey,
+    i: query,
+    
+  };
+  const queryString = formatQueryParams(params);
+  const url = searchIngredientURL + '?' + queryString;
+  
+  console.log(url);
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+    //   throw new Error(response.statusText);
+    })
+    .then(responseJson => displayIngredientResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text('There are no results matching. Please try again');
+    });
+}
+  
+
+
+function searchByName(query) {
+  const params = {
+    key: apiKey,
+    s: query,
+  };
+
+  const queryString = formatQueryParams(params);
+  const url = searchNameURL + '?' + queryString;
+    
+  console.log(url);
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+    //   throw new Error(response.statusText);
+    })
+    .then(responseJson => displayNameResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text('There are no results matching. Please try again');
+    });
+}
+
+
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    $('.results').empty();
+    
+    const searchTerm = $('#js-search-term').val();
+    const searchType = $('#js-search-type').val();
+    
+    if (searchType === 'ingredient'){
+      searchByIngredients(searchTerm);
+    }
+    else {
+      searchByName(searchTerm);
+    }
+  });
+}
+  
+$(watchForm);
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const apiKey = '1';
-const searchIngredientURL = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php';
 const searchNameURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php';
 
 function formatQueryParams(params) {
@@ -10,78 +9,43 @@ function formatQueryParams(params) {
   return queryItems.join('&');
 }
   
-function displayIngredientResults(responseJson) {
-
-  console.log(responseJson);
-  $('#results-list').empty();
-  for (let i = 0; i < responseJson.drinks.length; i++){   
-    $('#results-list').append(
-      `<li><h3>${responseJson.drinks[i].strDrink}</h3>
-          <img src="${responseJson.drinks[i].strDrinkThumb}">
-          </li>`
-    );}
-  
-  $('#results').removeClass('hidden');
-}
-function displayNameResults(responseJson) {
-
+function displayResults(responseJson) {
   console.log(responseJson);
   $('#results-list').empty();
   $('#js-error-message').empty();
   for (let i = 0; i < responseJson.drinks.length; i++){   
     $('#results-list').append(
-      `<h3>${responseJson.drinks[i].strDrink}</h3>
-            <img src="${responseJson.drinks[i].strDrinkThumb}">
+      `<div class="thumbs">
+      <h3>${responseJson.drinks[i].strDrink}</h3>
+            <a href="javascript:displayCocktail(responseJson)"><img src="${responseJson.drinks[i].strDrinkThumb}" class="drink-image"></a>
+            </div>
+            <div class="details" id="${responseJson.drinks[i].idDrink}">
+            <h3>Instructions:</h3>
+            <p>${responseJson.drinks[i].idDrink}</p>
+            <p>${responseJson.drinks[i].strInstructions}</p>
+            <ul>Ingredients:
+              <li>${responseJson.drinks[i].strIngredient1}</li>
+              <li>${responseJson.drinks[i].strIngredient2}</li>
+              <li>${responseJson.drinks[i].strIngredient3}</li>
+               <li>${responseJson.drinks[i].strIngredient4}</li>
+             </ui>
+             </div>
            `
     );}
-    
   $('#results').removeClass('hidden');
+  $('.details').hide();
 }
-// function displayCocktail(responseJson) {
-//     $('#results-list').empty();
-//     $('#results-drink').empty();
-//     for (let i = 0; i < responseJson.drinks.length; i++){   
-//       $('#results-drink').append(
-//         `<h3>${responseJson.drinks[i].strDrink}</h3>
-//             <img src="${responseJson.drinks[i].strDrinkThumb}">
-//             <ul>Ingredients:
-//               <li>${responseJson.drinks[i].strIngredient1}</li>
-//               <li>${responseJson.drinks[i].strIngredient2}</li>
-//               <li>${responseJson.drinks[i].strIngredient3}</li>
-//               <li>${responseJson.drinks[i].strIngredient4}</li>
-//             </ui>
-//             <h3>Instructions:</h3>
-//             <p>${responseJson.drinks[i].strInstructions}</p>`
-//       );}
-    
-//     $('#results-drink').removeClass('hidden');
-  
-//   }
-    
-function searchByIngredients(query) {
-  const params = {
-    key: apiKey,
-    i: query,
-    
-  };
-  const queryString = formatQueryParams(params);
-  const url = searchIngredientURL + '?' + queryString;
-  
-  console.log(url);
-  fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-    //   throw new Error(response.statusText);
-    })
-    .then(responseJson => displayIngredientResults(responseJson))
-    .catch(err => {
-      $('#js-error-message').text('There are no results matching. Please try again');
-    });
-}
-  
 
+function displayCocktail(responseJson){
+  $('#results-list').on('click', '.drink-image', event => {
+    // alert(event.target.id);
+    $('.details').toggle(1000);
+    
+    
+    
+
+  });
+}
 
 function searchByName(query) {
   const params = {
@@ -98,9 +62,9 @@ function searchByName(query) {
       if (response.ok) {
         return response.json();
       }
-    //   throw new Error(response.statusText);
     })
-    .then(responseJson => displayNameResults(responseJson))
+    .then(responseJson => displayResults(responseJson))
+    .then(responseJson => displayCocktail(responseJson))
     .catch(err => {
       $('#js-error-message').text('There are no results matching. Please try again');
     });
@@ -114,14 +78,8 @@ function watchForm() {
     $('.results').empty();
     
     const searchTerm = $('#js-search-term').val();
-    const searchType = $('#js-search-type').val();
+    searchByName(searchTerm);
     
-    if (searchType === 'ingredients'){
-      searchByIngredients(searchTerm);
-    }
-    else {
-      searchByName(searchTerm);
-    }
   });
 }
   
