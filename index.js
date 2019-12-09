0e76c6fe8808bb413cf49eff4c44cb56d680df45
Peter Rufo <peter@peterrufo.com>
2019-12-09 11:57:33 -0500

    styled the app

--- index.js
+++ index.js
@@ -10,47 +10,69 @@ function formatQueryParams(params) {
 }
   
 function displayResults(responseJson) {
-  console.log(responseJson);
   $('#results-list').empty();
   $('#js-error-message').empty();
   for (let i = 0; i < responseJson.drinks.length; i++){  
     $('#results-list').append(
       `<div class="thumbs">
-        <h3>${responseJson.drinks[i].strDrink}</h3>
-            <a href="javascript:displayCocktail(responseJson)"><img src="${responseJson.drinks[i].strDrinkThumb}" class="drink-image"></a>
-            
-            <div class="details" id="${responseJson.drinks[i].idDrink}">
-            <h3>${responseJson.drinks[i].strDrink}</h3>
-            <ul class="js-ul"><h3>Ingredients:</h3>
+        <h2>${responseJson.drinks[i].strDrink}</h2>
+        <a href="javascript:displayCocktail(responseJson)"><img src="${responseJson.drinks[i].strDrinkThumb}" class="drink-image"></a>
+        <div class="details" id="${responseJson.drinks[i].idDrink}">
+          <h2>${responseJson.drinks[i].strDrink}</h2>
+          <ul class="js-ul"><h3>Ingredients:</h3>
             ${Object.keys(responseJson.drinks[i])
             .filter(key => key.includes('strIngredient'))
             .map(key => {
             const value = responseJson.drinks[i][key];
             if (!value) {
-              return '';
-             }
+            return '';
+            }
             return '<li>' + value + '</li>';
             }).join('')
-}
+            }
             </ul>
           
             <h3>Instructions:</h3>
             <p>${responseJson.drinks[i].strInstructions}</p>
+            <h3>Serving Glass:</h3>
+            <p>${responseJson.drinks[i].strGlass}</p>
             </div>
       </div>
+      
       `
            
            
     );}
+  $('#results-list').append(
+    '<footer><a href="#top">Click here to go to top of page</a></footer>'   
+  );
   $('#results').removeClass('hidden');
   $('.details').hide();
+
+  $('#button').click(function() {
+    $('html, body').animate({
+      scrollTop: $('#results').offset().top
+    }, 1000);
+  });
+
+  
+
+  
 }
 
 function displayCocktail(responseJson){
-  $('#results-list').on('click', '.drink-image', function (event) {
+  $('#results-list').on('click', '.drink-image', function () {
     console.log($(this).parent().siblings()[1]);
-    $(this).parent().siblings().toggle(600);   
+    $(this).parent().siblings().toggle(600); 
+    
   });
+  $('#results-list').on('submit', '.drink-image', function () {
+    event.preventDefault();
+    console.log($(this).parent().siblings()[1]);
+    $(this).parent().siblings().toggle(600); 
+    
+  });
+
 }
 
 function searchByName(query) {
@@ -72,7 +94,7 @@ function searchByName(query) {
     .then(responseJson => displayResults(responseJson))
     .then(responseJson => displayCocktail(responseJson))
     .catch(err => {
-      $('#js-error-message').text('There are no results matching. Please try again');
+      $('#js-error-message').text('Couldn\'t find anything like that. Please try again');
     });
 }
 
